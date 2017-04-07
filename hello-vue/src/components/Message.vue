f08431cee310e0f4897997260cff8d2e56cd12a5
Prashant Khurana <prashant@prashantkhurana.com>
2017-04-06 21:18:39 -0400

    hello vue

---
+++ hello-vue/src/components/Message.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="test">
+<p>
+<slot></slot>
+</p>
+
+</div>
+</template>
+
+<<script>
+    
+    export default {
+        
+    }
+
+</script>
+
+<style>
+.box {background: #e3e3e3;}
+</style>
