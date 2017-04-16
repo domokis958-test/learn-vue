a628ec368694b8db2805f4415917405bf36f0bfe
Prashant Khurana <prashant@prashantkhurana.com>
2017-04-16 13:48:04 -0400

    one way of doing the the same thing.

--- vue-udemy/module-2/app4.js
+++ vue-udemy/module-2/app4.js
@@ -2,16 +2,10 @@ new Vue({
     el:'#app',
     data: {
         counter:0,
-        result: ''
     },
     methods: {
-        increase: function() {
-            this.counter++;
-            this.result = this.counter > 5 ? 'Greater 5' : 'Smaller 5';
-        },
-        decrease: function() {
-            this.counter--;
-            this.result = this.counter > 5 ? 'Greater 5' : 'Smaller 5';
+        result() {
+            return this.counter > 5 ? 'Greater 5' : 'Smaller 5';
         }
     }
 })
