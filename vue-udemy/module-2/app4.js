23f50e7a2cb4338c62c153295c6a87d75fcc6f4d
Prashant Khurana <prashant@prashantkhurana.com>
2017-04-16 14:14:27 -0400

    log to prove that computed are only called when required. code analysis is done

--- vue-udemy/module-2/app4.js
+++ vue-udemy/module-2/app4.js
@@ -6,12 +6,13 @@ new Vue({
     },
     methods: {
         result() {
-            console.log("conmputed method run");
+            console.log(" method run");
             return this.counter > 5 ? 'Greater 5' : 'Smaller 5';
         }
     },
     computed: {
         output() {
+            console.log("conmputed method run");
             return this.counter > 5 ? 'Greater 5' : 'Smaller 5';
         }
     }
