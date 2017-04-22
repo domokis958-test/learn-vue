9dab44ac11ea60eb1da8bb78832ec8a93a82bc2d
Prashant Khurana <prashant@prashantkhurana.com>
2017-04-21 22:17:34 -0400

    works but nice error
    Avoid mutating a prop directly since the value will be overwritten
    whenever the parent component re-renders. Instead, use a data or
    computed property based on the prop's value. Prop being mutated:
    "myName"

--- vue-udemy/module7/module7/src/components/UserDetail.vue
+++ vue-udemy/module7/module7/src/components/UserDetail.vue
@@ -3,23 +3,21 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>User Name : {{ switchName() }}</p>
+        <button @click="resetName">Reset name</button>
     </div>
 </template>
 
 <script>
     export default {
         props: {
-            myName: {
-                type:Object,
-                default:{
-                    name:'Max'
-                }
-            }
-                
+            myName: String
         },
         methods: {
             switchName() {
                 return this.myName.split("").reverse().join("");
+            },
+            resetName() {
+                this.myName = 'Max';
             }
         }
     }
