9bf480f2398450f950638dc3506158752ff0b9ff
Prashant Khurana <prashant@prashantkhurana.com>
2017-04-21 22:08:07 -0400

    requried value validation

--- vue-udemy/module7/module7/src/components/UserDetail.vue
+++ vue-udemy/module7/module7/src/components/UserDetail.vue
@@ -9,7 +9,11 @@
 <script>
     export default {
         props: {
-            myName: String
+            myName: {
+                type:String,
+                required:true,
+            }
+                
         },
         methods: {
             switchName() {
