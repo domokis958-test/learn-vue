3d551affe006a9d48ff4e364abac3e0c5436befc
Prashant Khurana <prashant@prashantkhurana.com>
2017-04-21 22:49:38 -0400

    passing a callback as a prop

--- vue-udemy/module7/module7/src/components/UserDetail.vue
+++ vue-udemy/module7/module7/src/components/UserDetail.vue
@@ -3,15 +3,18 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>User Name : {{ switchName() }}</p>
+        <button @click="resetFn()">Reset name</button>
         <button @click="resetName">Reset name</button>
+
     </div>
 </template>
 
 <script>
     export default {
         props: {
-            myName: String
-        },
+            myName: String,
+            resetFn: Function
+        }, 
         methods: {
             switchName() {
                 return this.myName.split("").reverse().join("");
