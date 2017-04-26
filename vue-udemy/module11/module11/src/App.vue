fca2ce24499abe0af11ba9377a03fcb4ed9a60c7
Prashant Khurana <prashant@prashantkhurana.com>
2017-04-26 18:15:26 -0400

    using vmodel to bind data

--- vue-udemy/module11/module11/src/App.vue
+++ vue-udemy/module11/module11/src/App.vue
@@ -7,24 +7,15 @@
                     <hr>
                     <div class="form-group">
                         <label for="email">Mail</label>
-                        <input
-                                type="text"
-                                id="email"
-                                class="form-control">
+                        <input type="text" id="email" class="form-control" v-model="userData.email">
                     </div>
                     <div class="form-group">
                         <label for="password">Password</label>
-                        <input
-                                type="password"
-                                id="password"
-                                class="form-control">
+                        <input type="password" id="password" class="form-control" v-model="userData.password">
                     </div>
                     <div class="form-group">
                         <label for="age">Age</label>
-                        <input
-                                type="number"
-                                id="age"
-                                class="form-control">
+                        <input type="number" id="age" class="form-control" v-model="userData.age">
                     </div>
 
                 </div>
@@ -33,10 +24,7 @@
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group">
                     <label for="message">Message</label><br>
                     <!-- Interpolation between <textarea>{{ test }}</textarea> doesn't work!-->
-                    <textarea
-                            id="message"
-                            rows="5"
-                            class="form-control"></textarea>
+                    <textarea id="message" rows="5" class="form-control"></textarea>
                 </div>
             </div>
             <div class="row">
@@ -77,9 +65,7 @@
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 from-group">
                     <label for="priority">Priority</label>
-                    <select
-                            id="priority"
-                            class="form-control">
+                    <select id="priority" class="form-control">
                         <option></option>
                     </select>
                 </div>
@@ -87,8 +73,7 @@
             <hr>
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <button
-                            class="btn btn-primary">Submit!
+                    <button class="btn btn-primary">Submit!
                     </button>
                 </div>
             </div>
@@ -101,9 +86,9 @@
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Mail:</p>
-                        <p>Password:</p>
-                        <p>Age:</p>
+                        <p>Mail: {{ userData.email }}</p>
+                        <p>Password:{{ userData.password }}</p>
+                        <p>Age: {{ userData.age }}</p>
                         <p>Message: </p>
                         <p><strong>Send Mail?</strong></p>
                         <ul>
@@ -121,7 +106,17 @@
 
 <script>
     export default {
+        data() {
+            return {
+                userData: {
+                    email : '',
+                    password: '',
+                    age: 27
+                }
+            }
+        }
     }
+
 </script>
 
 <style>
