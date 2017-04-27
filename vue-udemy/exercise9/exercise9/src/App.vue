5bd463bbdf92bbbcd8a34977c7ce8a40635daa25
Prashant Khurana <prashant@prashantkhurana.com>
2017-04-27 18:25:07 -0400

    exercicse 9 done .use of vmodel on custom components

--- vue-udemy/exercise9/exercise9/src/App.vue
+++ vue-udemy/exercise9/exercise9/src/App.vue
@@ -1,8 +1,26 @@
 <template>
     <div class="container">
-        <form>
+        <form v-if="!userData.submitted">
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <app-full-name v-model="userData.fullName"></app-full-name>
+                    <div class="form-group">
+                        <label for="email">Mail</label>
+                        <input type="text" id="email" class="form-control" v-model="userData.email">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" id="password" class="form-control" v-model.lazy="userData.password">
+                    </div>
+                    <div class="form-group">
+                        <label>
+                            <input type="radio" value="Yes" v-model="userData.storeData"> Yes
+                        </label>
+                        <label>
+                            <input type="radio" value="No" v-model="userData.storeData"> No
+                        </label>
+                    </div>
+                    <button type="submit" @click.prevent = "userData.submitted = true" class="btn btn-primary">Submit</button>
                     <!-- Exercise 1 -->
                     <!-- Create a Signup Form where you retrieve the following Information -->
                     <!-- Full Name (First Name + Last Name) -->
@@ -21,17 +39,17 @@
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="userData.submitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
+                        <p>Full Name: {{userData.firstName}} {{userData.lastName}}</p>
+                        <p>Mail: {{userData.email}} </p>
+                        <p>Password:{{userData.password}} </p>
+                        <p>Store in Database?: {{userData.storeData}} </p>
                     </div>
                 </div>
             </div>
@@ -40,7 +58,24 @@
 </template>
 
 <script>
+    import FullName from './Fullname.vue';
     export default {
+        data() {
+            return {
+                userData : {
+                    fullName : 'test vk',
+                    firstName : '',
+                    lastName : '',
+                    email : '',
+                    password : '',
+                    storeData : 'Yes',
+                    submitted : false
+                }
+            }
+        },
+        components: {
+            appFullName : FullName
+        }
     }
 </script>
 
