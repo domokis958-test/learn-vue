f4079b576c947588d73abcac45993a1ecf8e6e76
Prashant Khurana <prashant@prashantkhurana.com>
2017-04-27 18:57:39 -0400

    custom component

--- vue-udemy/module13/module13/src/App.vue
+++ vue-udemy/module13/module13/src/App.vue
@@ -3,7 +3,14 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Directives</h1>
-
+                <p v-text="'Some text'"></p>
+                <p v-html="'<strong> Some strong text </strong>'"></p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Custom Directives</h1>
+                <p v-highlight>Color this</p>
             </div>
         </div>
     </div>
