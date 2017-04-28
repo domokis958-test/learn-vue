0ec042cd7a749a6cf1b06f41e6726d09ed1cbb74
Prashant Khurana <prashant@prashantkhurana.com>
2017-04-27 23:38:01 -0400

    generic now

--- vue-udemy/exercise10/exercise10/src/App.vue
+++ vue-udemy/exercise10/exercise10/src/App.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Directives Exercise</h1>
-                <button v-customOn="clicked" class="btn btn-primary">Click me</button>
+                <button v-customOn:click="clicked" class="btn btn-primary">Click me</button>
                 <!-- Exercise -->
                 <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
 
@@ -17,9 +17,12 @@
         directives: {
             customOn: {
                 bind(el, binding) {
-                    el.onclick = function() {
-                        binding.value();
-                    }
+                    // el.onclick = function() {
+                    //     binding.value();
+                    // }
+                    const type = binding.arg;
+                    const fn = binding.value;
+                    el.addEventListener(type, fn);
                 }
             }
         },
