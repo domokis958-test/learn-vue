1733ce417cea73fa48c7e431051fb83e9ef87931
Prashant Khurana <prashant@prashantkhurana.com>
2017-04-28 00:17:43 -0400

    computed properties are alternatives to filters

--- vue-udemy/module13/module13/src/App.vue
+++ vue-udemy/module13/module13/src/App.vue
@@ -4,6 +4,11 @@
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Filters & Mixins</h1>
                 <p>{{ text | toUppercase | toLowercase }}</p>
+                <hr>
+                <input v-model="filterText">
+                <ul>
+                    <li v-for="fruit in filteredFruits">{{fruit}}</li>
+                </ul>
 
             </div>
         </div>
@@ -14,12 +19,21 @@
     export default {
         data() {
             return {
-                text : 'Hello There'
+                text : 'Hello There',
+                fruits : ['Apple', 'Banana', 'Mango', 'Melon'],
+                filterText : ''
             }
         },
         filters: {
             toUppercase(value) {
                 return value.toUpperCase();
+            }
+        },
+        computed : {
+            filteredFruits() {
+                return this.fruits.filter((element)=> {
+                    return element.match(this.filterText);
+                })
             }
         }
     }
