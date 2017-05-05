b280bd85607445c236364d4988ace09a5a67fff0
Prashant Khurana <prashant@prashantkhurana.com>
2017-05-04 23:24:35 -0400

    transitios. enter leave enter etc

--- vue-udemy/module14/module14/src/App.vue
+++ vue-udemy/module14/module14/src/App.vue
@@ -6,8 +6,7 @@
                 <hr>
                 <button class="btn btn-primary" @click="show = !show">Show Alert</button>
                 <br> <br>
-                <transition>
-                    <div class="alert alert-info" v-if="show"> This is some info</div>
+                <transition name="fade">
                     <div class="alert alert-info" v-if="show"> This is some info</div>
                 </transition>
 
@@ -29,5 +28,24 @@
 </script>
 
 <style>
+    .fade-enter {
+        opacity: 0;
+
+    }
+    .fade-enter-active {
+        transition: opacity 1s;
+        /*opacity: 1 not required as it is the default and the fade enter will be removed after 1 frame actually will overide the other. so dont do this*/
+
+
+    }
+    .fade-leave {
+        /*opacity: 1 not required*/
+
+    }
+    .fade-leave-active {
+        transition: opacity 1s;
+        opacity: 0;
+
+    }
 
 </style>
