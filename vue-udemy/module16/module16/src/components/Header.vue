4e752219547fd997d06ed537c02f3d7d4e022b88
Prashant Khurana <prashant@prashantkhurana.com>
2017-05-05 22:53:07 -0400

    using router link instead of anchor tag

---
+++ vue-udemy/module16/module16/src/components/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <ul class="nav nav-pills">
+            <li role="presentation"><router-link to="/">Home</router-link></li>
+            <li role="presentation"><router-link to="/user">User</router-link></li>
+        </ul>
+    </div>
+</template>
