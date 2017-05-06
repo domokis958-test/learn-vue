eb65ee513a7d8797474f7c8c1c1fe7d1c296d378
Prashant Khurana <prashant@prashantkhurana.com>
2017-05-05 23:01:39 -0400

    navigate between pages between pages

--- vue-udemy/module16/module16/src/components/user/User.vue
+++ vue-udemy/module16/module16/src/components/user/User.vue
@@ -1,4 +1,20 @@
 <template>
-    <h1>The User Page</h1>
-
+<div>
+    <h1>The user page</h1>
+    <hr>
+    <button @click="navigateToHome">Go to home</button>
+</div>
 </template>
+
+<script>
+    export default {
+        methods : {
+            navigateToHome() {
+                this.$router.push({
+                    path: '/'
+                })
+                
+            }
+        }
+    }
+</script>
