f5a803bd4e63e2c2ef8c85ecb3c4429e86fb4baf
Prashant Khurana <prashant@prashantkhurana.com>
2017-05-08 01:50:19 -0400

    lazy loading

--- vue-udemy/module16/module16/src/routes.js
+++ vue-udemy/module16/module16/src/routes.js
@@ -1,24 +1,47 @@
-import User from './components/user/User.vue'
-import UserStart from './components/user/UserStart.vue'
-import UserDetail from './components/user/UserDetail.vue'
-import UserEdit from './components/user/UserEdit.vue'
+// import User from './components/user/User.vue'
+// import UserStart from './components/user/UserStart.vue'
+// import UserDetail from './components/user/UserDetail.vue'
+// import UserEdit from './components/user/UserEdit.vue'
 import Home from './components/Home.vue'
 import Header from './components/Header.vue'
 
-export const routes =  [
+
+const User = resolve => {
+    require.ensure(['./components/User/User.vue'], () => {
+        resolve(require('./components/User/User.vue'));
+    })
+}
+
+const UserStart = resolve => {
+    require.ensure(['./components/User/UserStart.vue'], () => {
+        resolve(require('./components/User/UserStart.vue'));
+    })
+}
+const UserEdit = resolve => {
+    require.ensure(['./components/User/UserEdit.vue'], () => {
+        resolve(require('./components/User/UserEdit.vue'));
+    })
+}
+const UserDetail = resolve => {
+    require.ensure(['./components/User/UserDetail.vue'], () => {
+        resolve(require('./components/User/UserDetail.vue'));
+    })
+}
+
+export const routes = [
     {
-        path : '/user',
-        components : {
-            default : User,
-            'header-bottom' : Header
+        path: '/user',
+        components: {
+            default: User,
+            'header-bottom': Header
         },
         children: [
             {
-                path : '/',
+                path: '/',
                 component: UserStart
             },
             {
-                path : ':id',
+                path: ':id',
                 component: UserDetail,
                 beforeEnter: (to, from, next) => {
                     console.log("inside route setup");
@@ -26,27 +49,27 @@ export const routes =  [
                 }
             },
             {
-                path : ':id/edit',
+                path: ':id/edit',
                 component: UserEdit,
-                name:'userEdit'
+                name: 'userEdit'
             }
         ]
     },
     {
-        path : '',
-        components : {
-            default : Home,
-            'header-top' : Header
+        path: '',
+        components: {
+            default: Home,
+            'header-top': Header
         },
         name: 'home'
     },
     {
-        path : '/redirect-me',
-        redirect : '/user'
+        path: '/redirect-me',
+        redirect: '/user'
     },
     {
-        path : '*',
-        redirect : '/'
+        path: '*',
+        redirect: '/'
     }
 
 ];
