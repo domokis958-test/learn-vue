9b3cb30bc6975db6452aa7fe6cd4eb136264b582
Prashant Khurana <prashant@prashantkhurana.com>
2017-05-07 22:09:40 -0400

    passing hash fragments

--- vue-udemy/module16/module16/src/components/user/UserEdit.vue
+++ vue-udemy/module16/module16/src/components/user/UserEdit.vue
@@ -3,6 +3,8 @@
         <h3>Edit the User</h3>
     <p>user has locale {{  $route.query.locale }}</p>
         <p>user has q {{  $route.query.q }}</p>
+        <div style="height : 700px"></div>
+        <p id="data">Some extra data</p>
 
     </div>
     
