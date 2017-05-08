78d10b66326ec19f56b301f1416c15834ecc7e40
Prashant Khurana <prashant@prashantkhurana.com>
2017-05-07 23:29:32 -0400

    access control before leaving a route

--- vue-udemy/module16/module16/src/components/user/UserEdit.vue
+++ vue-udemy/module16/module16/src/components/user/UserEdit.vue
@@ -1,12 +1,47 @@
 <template>
     <div>
         <h3>Edit the User</h3>
-    <p>user has locale {{  $route.query.locale }}</p>
-        <p>user has q {{  $route.query.q }}</p>
-        <button class="btn btn-primary">Confirm</button>
+        <p>user has locale {{ $route.query.locale }}</p>
+        <p>user has q {{ $route.query.q }}</p>
+        <button class="btn btn-primary" 
+        @click="test"
+        >Confirm</button>
+        <p>{{confirmed}}
+        </p>
         <div style="height : 700px"></div>
         <p id="data">Some extra data</p>
 
     </div>
-    
+
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                confirmed: false
+            }
+
+        },
+        methods: {
+            test() {
+                this.confirmed = true;
+                console.log(this.confirmed);
+            }
+
+        },
+        beforeRouteLeave(to, from, next) {
+            console.log(this.confirmed);
+            if (this.confirmed) { // checking if user is authenticated. here just making it true
+                next();
+            } else {
+                if (confirm('Are you sure')) {
+                    next();
+                } else {
+                    next(false);
+                }
+            }
+        }
+    }
+
+</script>
