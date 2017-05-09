75a29a491b0ddd9356f1d94342e8bd1fbe834c98
Prashant Khurana <prashant@prashantkhurana.com>
2017-05-08 21:12:45 -0400

    breaking out further

--- vue-udemy/module17/module17/src/store/store.js
+++ vue-udemy/module17/module17/src/store/store.js
@@ -1,6 +1,9 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import counter from './modules/counter'
+import counter from './modules/counter';
+import * as actions from './actions';
+import * as getters from './getters';
+import * as mutations from './mutations';
 
 Vue.use(Vuex);
 
@@ -8,28 +11,9 @@ export const store = new Vuex.Store({
     state: {
         value : 0
     },
-    getters: {
-        value : state => {
-            return state.value;
-        }
-    },
-    mutations: {
-        updateValue: (state, payload) => {
-            state.value = payload;
-        }
-    },
-    actions: {
-        // 2 ways of doing this. 
-        // increment: context => {
-        //     context.commit('increment');
-        // },
-
-        
-        updateValue : ({commit}, payload) => {
-            commit('updateValue', payload);
-        }
-
-    },
+    getters,
+    mutations,
+    actions,
     modules: {
         counter
     }
